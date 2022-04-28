4ea5925653a9d030658a2311ee4fb1ba8ba37166
FilipeAbdo <filipeibraim@gmail.com>
2022-04-28 19:15:03 -0300

    Updated to draw a tri-chromatic triangle

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,27 +1,105 @@
 use sdl2::keyboard::Keycode;
 
+
+extern crate gl;
 extern crate sdl2;
+
+pub mod render_gl;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
     let window = video_subsystem
-        .window("Prototype", 900, 700)
+        .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
 
-    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-    let window = video_subsystem.window("game tutorial", 800, 600)
-        .position_centered()
-        .build()
-        .expect("could not initialize video subsystem");
+    use std::ffi::CString;
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
+            .unwrap();
 
-    let mut canvas = window.into_canvas().build()
-        .expect("could not make a canvas");
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
+            .unwrap();
 
-    let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("cars/car1.png")?;
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+
+    shader_program.set_used();
+
+    let vertices: Vec<f32> = vec![
+        // positions      // colors
+        0.5, -0.5, 0.0,   1.0, 0.0, 0.0,   // bottom right
+        -0.5, -0.5, 0.0,  0.0, 1.0, 0.0,   // bottom left
+        0.0,  0.5, 0.0,   0.0, 0.0, 1.0    // top
+    ];
+            
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+    }
+    
+    unsafe {
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER, // target
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
+            gl::STATIC_DRAW, // usage
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+    }
+
+    unsafe {
+        gl::BindVertexArray(vao);
+        
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    
+        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            0, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            std::ptr::null() // offset of the first component
+        );
+
+        gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            1, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid // offset of the first component
+        );
+    
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+    unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -43,16 +121,24 @@ fn main() {
                 sdl2::event::Event::KeyUp { keycode: Some(Keycode::B), ..} => println!("Keycode B"),
                 _ => {},
             }
-            // match event {
-            //    sdl2::event::Event::KeyUp { keycode, ..} => {
-            //        println!("Keycode {:?}", keycode);
-            //        keycodeValue = keycode;
-            //     },
-            //     _ => {},
-            // }
+
+            unsafe {
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+            }
+    
+            shader_program.set_used();
+            unsafe {
+                gl::BindVertexArray(vao);
+                gl::DrawArrays(
+                    gl::TRIANGLES, // mode
+                    0, // starting index in the enabled arrays
+                    3 // number of indices to be rendered
+                );
+            }
+
+
+            window.gl_swap_window();
         }
-        // println!("keycode value = {:?}", keycodeValue);
-        // render window contents here
     }
 }
 
@@ -81,16 +167,16 @@ impl Default for KeyEvent {
 }
 
 fn detect_pressing(key:Keycode, up_down:KeyStatus) -> KeyEvent{
-    static mut lastKey:KeyInfo = KeyInfo{ key:None, status:KeyStatus::UP};
+    static mut LAST_KEY:KeyInfo = KeyInfo{ key:None, status:KeyStatus::UP};
     let mut return_value:KeyEvent = KeyEvent::NotPressed;
     unsafe{
-        if lastKey.key == Some(key) && matches!(up_down, KeyStatus::UP) && matches!(lastKey.status, KeyStatus::DOWN) {
+        if LAST_KEY.key == Some(key) && matches!(up_down, KeyStatus::UP) && matches!(LAST_KEY.status, KeyStatus::DOWN) {
             return_value = KeyEvent::Pressed;
         }else{
             return_value = KeyEvent::NotPressed;
         }
-        lastKey.key = Some(key);
-        lastKey.status = up_down;
+        LAST_KEY.key = Some(key);
+        LAST_KEY.status = up_down;
     }
     return  return_value;
 }
